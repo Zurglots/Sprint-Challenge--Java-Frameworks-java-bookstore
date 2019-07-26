d2f2d8c819c7496ecf25244c193df9e7331a2ce7
Zurglots <bfeole@gmail.com>
2019-07-26 12:27:48 -0400

    Added exception and logger details for listall endpoints

--- StartHere/src/main/java/com/lambdaschool/starthere/controllers/AuthorController.java
+++ StartHere/src/main/java/com/lambdaschool/starthere/controllers/AuthorController.java
@@ -1,7 +1,9 @@
 package com.lambdaschool.starthere.controllers;
 
 import com.lambdaschool.starthere.models.Author;
+import com.lambdaschool.starthere.models.ErrorDetail;
 import com.lambdaschool.starthere.services.AuthorService;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
@@ -20,7 +22,22 @@ public class AuthorController
 {
     @Autowired
     private AuthorService authorService;
+    @ApiOperation(value = "Returns all Authors with Paging Ability", responseContainer = "List")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "page", dataType = "integr", paramType = "query",
+                    value = "Results page you want to retrieve (0..N)"),
+            @ApiImplicitParam(name = "size", dataType = "integer", paramType = "query",
+                    value = "Number of records per page."),
+            @ApiImplicitParam(name = "sort", allowMultiple = true, dataType = "string", paramType = "query",
+                    value = "Sorting criteria in the format: property(,asc|desc). " +
+                            "Default sort order is ascending. " +
 
+                            "Multiple sort criteria are supported.")})
+
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Student Found", response = Author.class),
+            @ApiResponse(code = 404, message = "Student Not Found", response = ErrorDetail.class)
+    })
 
     @GetMapping(value = "/authors",
                 produces = {"application/json"})
--- StartHere/src/main/java/com/lambdaschool/starthere/controllers/BookController.java
+++ StartHere/src/main/java/com/lambdaschool/starthere/controllers/BookController.java
@@ -2,7 +2,11 @@ package com.lambdaschool.starthere.controllers;
 
 
 import com.lambdaschool.starthere.models.Book;
+import com.lambdaschool.starthere.models.ErrorDetail;
 import com.lambdaschool.starthere.services.BookService;
+import io.swagger.annotations.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
@@ -19,9 +23,28 @@ import java.util.List;
 @RequestMapping("/books")
 public class BookController
 {
+
+    private static final Logger logger = LoggerFactory.getLogger(BookController.class);
+
     @Autowired
     private BookService bookService;
 
+    @ApiOperation(value = "Returns all Books with Paging Ability", responseContainer = "List")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "page", dataType = "integr", paramType = "query",
+                    value = "Results page you want to retrieve (0..N)"),
+            @ApiImplicitParam(name = "size", dataType = "integer", paramType = "query",
+                    value = "Number of records per page."),
+            @ApiImplicitParam(name = "sort", allowMultiple = true, dataType = "string", paramType = "query",
+                    value = "Sorting criteria in the format: property(,asc|desc). " +
+                            "Default sort order is ascending. " +
+
+                            "Multiple sort criteria are supported.")})
+
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Student Found", response = Book.class),
+            @ApiResponse(code = 404, message = "Student Not Found", response = ErrorDetail.class)
+    })
     @GetMapping(value = "/books",
                 produces = {"application/json"})
     public ResponseEntity<?> listAllBooks(@PageableDefault(page = 0, size = 3) Pageable pageable, HttpServletRequest request)
