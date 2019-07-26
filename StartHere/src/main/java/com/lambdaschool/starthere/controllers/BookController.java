0c6cba5f21ef7fadda4959c5ceef05ba24dce7cd
Zurglots <bfeole@gmail.com>
2019-07-26 12:04:27 -0400

    Added endpoint for all books

--- StartHere/src/main/java/com/lambdaschool/starthere/controllers/BookController.java
+++ StartHere/src/main/java/com/lambdaschool/starthere/controllers/BookController.java
@@ -1,14 +1,33 @@
 package com.lambdaschool.starthere.controllers;
 
 
+import com.lambdaschool.starthere.models.Book;
+import com.lambdaschool.starthere.services.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @RestController
 @RequestMapping("/books")
 public class BookController
 {
-    
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping(value = "/books",
+                produces = {"application/json"})
+    public ResponseEntity<?> listAllBooks(@PageableDefault(page = 0, size = 3) Pageable pageable, HttpServletRequest request)
+    {
+        List<Book> myBooks = bookService.findAll(pageable);
+        return new ResponseEntity<>(myBooks, HttpStatus.OK);
+    }
 
 }
